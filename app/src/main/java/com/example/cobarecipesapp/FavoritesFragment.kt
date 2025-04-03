c7d35185dd7fcc88e4e7fa92cc03204c011698ef
COBaOrNotCOBa <yureal1986@mail.ru>
2025-04-03 20:44:09 +0300

    RA-05 Похоже на правду. Также добавил _binding в MainActivity

--- app/src/main/java/com/example/cobarecipesapp/FavoritesFragment.kt
+++ app/src/main/java/com/example/cobarecipesapp/FavoritesFragment.kt
@@ -5,12 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.cobarecipesapp.databinding.FragmentListCategoriesBinding
+import com.example.cobarecipesapp.databinding.FragmentFavoritesBinding
 import java.lang.IllegalStateException
 
-class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
+class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
 
-    private var _binding: FragmentListCategoriesBinding? = null
+    private var _binding: FragmentFavoritesBinding? = null
     private val binding
         get() = _binding ?: throw IllegalStateException("Binding is null")
 
@@ -19,7 +19,7 @@ class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
--- app/src/main/java/com/example/cobarecipesapp/MainActivity.kt
+++ app/src/main/java/com/example/cobarecipesapp/MainActivity.kt
@@ -4,22 +4,35 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.example.cobarecipesapp.databinding.ActivityMainBinding
+import java.lang.IllegalStateException
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding is null")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add(R.id.mainContainer, CategoriesListFragment())
 
+            }
+        }
+
+        binding.btnCategories.setOnClickListener {
+
+        }
+
+        binding.btnFavourites.setOnClickListener {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace(R.id.mainContainer, FavoritesFragment())
             }
         }
     }
